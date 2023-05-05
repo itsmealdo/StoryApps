187970bc9d220fd2dd051011660c0493b96d6fbd
itsmealdo <aldosaputra.gooo@gmail.com>
2023-05-05 11:37:28 +0700

    add Response

--- app/src/main/java/com/itsmealdo/storyapps/data/remote/network/ApiService.kt
+++ app/src/main/java/com/itsmealdo/storyapps/data/remote/network/ApiService.kt
@@ -1,11 +1,20 @@
 package com.itsmealdo.storyapps.data.remote.network
 
-import android.telecom.Call
+import com.itsmealdo.storyapps.data.remote.model.AddStoryResponse
+import com.itsmealdo.storyapps.data.remote.model.LoginResponse
+import com.itsmealdo.storyapps.data.remote.model.RegisterResponse
+import com.itsmealdo.storyapps.data.remote.model.StoryResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface ApiService {
     @FormUrlEncoded
@@ -26,5 +35,15 @@ interface ApiService {
     @GET("stories")
     fun getStoryData(
         @Header("Authorization") token: String,
-    )
+        @Query("page") page: Int? = null,
+        @Query("size") size: Int? = null
+    ): Call<StoryResponse>
+
+    @Multipart
+    @POST("stories")
+    fun addStory(
+        @Header("Authorization") token: String,
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+    ): Call<AddStoryResponse>
 }
